79d4a858d332e4632f1c16b97aec15a27402fce2
GHuyHuynh <huy.huynh@dal.ca>
2025-05-02 00:20:12 -0300

    remove deps

--- dashboard/src/lib/azure/azure-openai.ts
+++ dashboard/src/lib/azure/azure-openai.ts
@@ -1,15 +1,15 @@
-import { createAzure } from '@ai-sdk/azure';
+// import { createAzure } from '@ai-sdk/azure';
 
-const azure_resource_name = process.env.AZURE_RESOURCE_NAME || null;
-const azure_api_key = process.env.AZURE_API_KEY || null;
-const azure_api_version = process.env.AZURE_API_VERSION || null;
+// const azure_resource_name = process.env.AZURE_RESOURCE_NAME || null;
+// const azure_api_key = process.env.AZURE_API_KEY || null;
+// const azure_api_version = process.env.AZURE_API_VERSION || null;
 
-if (!azure_resource_name || !azure_api_key || !azure_api_version) {
-  throw new Error('AZURE_RESOURCE_NAME and AZURE_API_KEY and AZURE_API_VERSION must be set');
-}
+// if (!azure_resource_name || !azure_api_key || !azure_api_version) {
+//   throw new Error('AZURE_RESOURCE_NAME and AZURE_API_KEY and AZURE_API_VERSION must be set');
+// }
 
-export const azure = createAzure({
-  resourceName: azure_resource_name,
-  apiKey: azure_api_key,
-  apiVersion: azure_api_version,
-});
+// export const azure = createAzure({
+//   resourceName: azure_resource_name,
+//   apiKey: azure_api_key,
+//   apiVersion: azure_api_version,
+// });
