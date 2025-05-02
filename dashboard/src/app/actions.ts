b7716e9a00c3dc474b69a2982f8d540abb6efdf7
GHuyHuynh <huy.huynh@dal.ca>
2025-05-02 03:26:16 -0300

    add base chat

--- dashboard/src/app/actions.ts
+++ dashboard/src/app/actions.ts
@@ -1,17 +1,31 @@
-'use server'
+'use server';
 
-import { createClient } from '@/lib/supabase/server'
+import { createStreamableValue } from 'ai/rsc';
+import { CoreMessage, streamText } from 'ai';
+import { createAzure } from '@ai-sdk/azure';
 
-export async function getAllPersonaResponses() {
-  const supabase = await createClient()
-  
-  const { data, error } = await supabase
-    .from('persona_responses')
-    .select('*')
-  
-  if (error) {
-    throw new Error(`Error fetching persona responses: ${error.message}`)
-  }
-  
-  return data
+const azure_resource_name = process.env.AZURE_RESOURCE_NAME!;
+const azure_api_key = process.env.AZURE_API_KEY!;
+const azure_api_version = process.env.AZURE_API_VERSION!;
+
+const azure = createAzure({
+  resourceName: azure_resource_name,
+  apiKey: azure_api_key,
+  apiVersion: azure_api_version,
+});
+
+export interface Message {
+  role: 'user' | 'assistant';
+  content: string;
+}
+
+// Streaming Chat 
+export async function continueTextConversation(messages: CoreMessage[]) {
+  const result = await streamText({
+    model: azure('gpt-4o-mini'),
+    messages,
+  });
+
+  const stream = createStreamableValue(result.textStream);
+  return stream.value;
 }
