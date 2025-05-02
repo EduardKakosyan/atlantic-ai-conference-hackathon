7b1509ad820edcd7b0d4d198bd8e349da4131b81
GHuyHuynh <huy.huynh@dal.ca>
2025-05-02 00:08:07 -0300

    move azure

--- dashboard/src/lib/azure/azure-openai.ts
+++ dashboard/src/lib/azure/azure-openai.ts
@@ -1,8 +1,8 @@
 import { createAzure } from '@ai-sdk/azure';
 
-const azure_resource_name = process.env.AZURE_RESOURCE_NAME;
-const azure_api_key = process.env.AZURE_API_KEY;
-const azure_api_version = process.env.AZURE_API_VERSION;
+const azure_resource_name = process.env.AZURE_RESOURCE_NAME || null;
+const azure_api_key = process.env.AZURE_API_KEY || null;
+const azure_api_version = process.env.AZURE_API_VERSION || null;
 
 if (!azure_resource_name || !azure_api_key || !azure_api_version) {
   throw new Error('AZURE_RESOURCE_NAME and AZURE_API_KEY and AZURE_API_VERSION must be set');
