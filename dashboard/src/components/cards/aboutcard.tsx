b4b27b8cc8baa7ea50eb8694fcc4abb024b57fb3
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2025-05-05 19:43:18 -0300

    feat: making chat content dynamic

--- dashboard/src/components/cards/aboutcard.tsx
+++ dashboard/src/components/cards/aboutcard.tsx
@@ -6,17 +6,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import personasData from "@/lib/data/personas.json"
 
-export default function AboutCard() {
+export default function AboutCard({
+  id
+ }: {
+    id: string 
+ }) {
+
+  const personaId = parseInt(id, 10)
+  const persona = personasData.find(p => p.persona_id === personaId)
+
   return (
     <div className="max-w-xl mx-auto mt-10">
       <Card>
         <CardHeader>
           <CardTitle>Conversation Comparison</CardTitle>
-          <CardDescription>A conversation comparison tool for the AI profile of David</CardDescription>
+          <CardDescription>A conversation comparison tool for the AI profile of {`${persona?.name}`}</CardDescription>
         </CardHeader>
         <CardContent className="text-sm text-muted-foreground/90 leading-normal prose"> 
-          <p className="mb-3">Compare the before and after exposing to news about COVID-19 vaccines of the AI profile of David</p>
+          <p className="mb-3">Compare the before and after exposing to news about COVID-19 vaccines of the AI profile of {`${persona?.name}`}</p>
           
         </CardContent>
       </Card>
