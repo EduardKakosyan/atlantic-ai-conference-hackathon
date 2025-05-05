dfad4a688786798648c452bc41af8bfbc425f579
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2025-05-05 20:05:05 -0300

    feat: finished dynamic chat

--- dashboard/src/components/cards/aboutcard.tsx
+++ dashboard/src/components/cards/aboutcard.tsx
@@ -4,31 +4,33 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Link from "next/link";
-import personasData from "@/lib/data/personas.json"
+} from "@/components/ui/card"
+import { type Persona } from "@/components/chat"
 
-export default function AboutCard({
-  id
- }: {
-    id: string 
- }) {
+interface AboutCardProps {
+  persona: Persona
+}
 
-  const personaId = parseInt(id, 10)
-  const persona = personasData.find(p => p.persona_id === personaId)
+export default function AboutCard({ persona }: AboutCardProps) {
+  //extracting first name 
+  const displayName = persona.name.split(' ')[0]
 
   return (
     <div className="max-w-xl mx-auto mt-10">
       <Card>
         <CardHeader>
           <CardTitle>Conversation Comparison</CardTitle>
-          <CardDescription>A conversation comparison tool for the AI profile of {`${persona?.name}`}</CardDescription>
+          <CardDescription>
+            A conversation comparison tool for the AI profile of {displayName}
+          </CardDescription>
         </CardHeader>
-        <CardContent className="text-sm text-muted-foreground/90 leading-normal prose"> 
-          <p className="mb-3">Compare the before and after exposing to news about COVID-19 vaccines of the AI profile of {`${persona?.name}`}</p>
-          
+        <CardContent className="text-sm text-muted-foreground/90 leading-normal prose">
+          <p className="mb-3">
+            Compare the before and after exposing to news about COVID-19 vaccines for{' '}
+            <strong>{displayName}</strong>.
+          </p>
         </CardContent>
       </Card>
     </div>
   )
-}
+} 
